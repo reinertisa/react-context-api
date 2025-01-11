1436cb0e6a9b07a326d3036f795003679e9ed687
Isa Reinert <reinert.isa@gmail.com>
2025-01-11 10:38:26 -0800

    feat(website): add song list

---
+++ src/components/SongList.js
@@ -0,0 +1,13 @@
+
+
+export default function SongList() {
+
+    return (
+        <div className="song-list">
+            <ul>
+                <li>this wild darkness</li>
+                <li>memory gospel</li>
+            </ul>
+        </div>
+    )
+}
