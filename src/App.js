5799356d08f8b8a2dab5f01469f0be2a8dcc215a
Isa Reinert <reinert.isa@gmail.com>
2025-01-12 12:36:54 -0800

    fix(website): update App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import SongList from "./components/SongList";
+import Navbar from "./components/Navbar";
+import ThemeContextProvider from "./contexts/ThemeContext";
+import AuthContextProvider from "./contexts/AuthContext";
+import SongContextProvider from "./contexts/SongContext";
+import NewSongForm from "./components/SongForm";
+
 
 function App() {
-  return (
-    <div className="App">
-      <SongList />
-    </div>
-  );
+    return (
+        <div className="App">
+            <ThemeContextProvider>
+                <AuthContextProvider>
+                    <Navbar />
+                    <SongContextProvider>
+                        <SongList />
+                        <NewSongForm />
+                    </SongContextProvider>
+                </AuthContextProvider>
+            </ThemeContextProvider>
+        </div>
+    );
 }
 
 export default App;
